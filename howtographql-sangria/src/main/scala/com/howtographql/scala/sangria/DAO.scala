3e0cf26e161ca6d470b99b10b775e0a1e9ba8cc2
projectormato <k.keita811.anuy@gmail.com>
2018-09-13 14:34:12 +0900

    add 'Handling Arguments' chapter content.

--- howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DAO.scala
+++ howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/DAO.scala
@@ -1,7 +1,19 @@
 package com.howtographql.scala.sangria
 import DBSchema._
 import slick.jdbc.H2Profile.api._
+// add "Handling Arguments" chapter
+import com.howtographql.scala.sangria.models.Link
+import scala.concurrent.Future
 
 class DAO(db: Database) {
   def allLinks = db.run(Links.result)
+
+  // add "Handling Arguments" chapter
+  def getLink(id: Int): Future[Option[Link]] = db.run(
+     Links.filter(_.id === id).result.headOption
+   )
+
+   def getLinks(ids: Seq[Int]) = db.run(
+     Links.filter(_.id inSet ids).result
+   )
 }
--- howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
+++ howtographql-sangria/src/main/scala/com/howtographql/scala/sangria/GraphQLSchema.scala
@@ -1,31 +1,34 @@
 package com.howtographql.scala.sangria
 
-import sangria.schema.{Field, ListType, ObjectType}
+import sangria.schema.{ListType, ObjectType}
 import models._
-// #
 import sangria.schema._
 import sangria.macros.derive._
 
 object GraphQLSchema {
 
-  // 1
-  val LinkType = ObjectType[Unit, Link](
-       "Link",
-       fields[Unit, Link](
-         Field("id", IntType, resolve = _.value.id),
-         Field("url", StringType, resolve = _.value.url),
-         Field("description", StringType, resolve = _.value.description)
-       )
-     )
+  implicit val LinkType = deriveObjectType[Unit, Link]()
 
-  // 2
+
+  val Id = Argument("id", IntType)
+  val Ids = Argument("ids", ListInputType(IntType))
+
   val QueryType = ObjectType(
     "Query",
     fields[MyContext, Unit](
-      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks)
+      Field("allLinks", ListType(LinkType), resolve = c => c.ctx.dao.allLinks),
+      Field("link",
+        OptionType(LinkType),
+        arguments = Id :: Nil,
+        resolve = c => c.ctx.dao.getLink(c.arg(Id))
+      ),
+      Field("links",
+        ListType(LinkType),
+        arguments = Ids :: Nil,
+        resolve = c => c.ctx.dao.getLinks(c.arg(Ids))
+      )
     )
   )
 
-  // 3
   val SchemaDefinition = Schema(QueryType)
 }
